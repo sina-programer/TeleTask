aa30e743c3c7af1941bebbec098afee3f14b6f5a
sina <sina.programerr@gmail.com>
2022-07-15 17:47:31 +0430

    Clean app.py

--- app.py
+++ app.py
@@ -12,24 +12,20 @@ def create_ch_group():
     return jsonify('ok')
 
     if 'task_type' not in request_data:
-        data = {'message': 'Task type can not be empty'}
-        return jsonify(data)
+        return jsonify({'message': 'Task type can not be empty'})
 
     task_type = int(request_data['task_type'])
-    if request.method == 'POST':
 
-        if (task_type == 1):
+    if request.method == 'POST':
+        if task_type == 1:
             if 'username' not in request_data:
-                data = {'message': 'username can not be empty'}
-                return jsonify(data)
+                return jsonify({'message': 'username can not be empty'})
 
             if 'phone_number' not in request_data:
-                data = {"message": 'phone number can not be empty'}
-                return jsonify(data)
+                return jsonify({"message": 'phone number can not be empty'})
 
             if 'channel_title' not in request_data:
-                data = {"message": 'channel can not be empty'}
-                return jsonify(data)
+                return jsonify({"message": 'channel can not be empty'})
 
             username = request_data['username']
             phone_number = request_data['phone_number']
@@ -37,16 +33,13 @@ def create_ch_group():
             channel_bio = request_data['channel_bio'] if 'channel_bio' in request_data else ''
 
             if len(username) == 0:
-                data = {"message": 'username can not be an empty string'}
-                return jsonify(data)
+                return jsonify({"message": 'username can not be an empty string'})
 
             if len(phone_number) == 0:
-                data = {"message": 'phone_number can not be an empty string'}
-                return jsonify(data)
+                return jsonify({"message": 'phone_number can not be an empty string'})
 
             if len(channel_title) == 0:
-                data = {"message": 'channel_title can not be an empty string'}
-                return jsonify(data)
+                return jsonify({"message": 'channel_title can not be an empty string'})
 
             inserted_id = actions_collection.insert_one({
                 "username": username,
@@ -57,91 +50,89 @@ def create_ch_group():
                 'status': 'waiting',
                 'insert_date': datetime.now()
             }).inserted_id
-            if inserted_id:
-                response = make_response(
-                    jsonify(
-                        {"message": '201 Channel created', "severity": "info"}
-                    ),
-                    201,
-                )
-            else:
-                response = make_response(
-                    jsonify(
-                        {"message": '500 Channel not created', "severity": "danger"}
-                    ),
-                    500,
-                )
-        if (task_type == 2):
-            if 'group_title' not in request_data:
-                data = {"message": 'Group title can not be empty'}
-                return jsonify(data)
-
-            if 'username' not in request_data:
-                data = {"message": 'username can not be empty'}
-                return jsonify(data)
-
-            if 'phone_number' not in request_data:
-                data = {"message": 'Phone number can not be empty'}
-                return jsonify(data)
-
-            username = request_data['username']
-            phone_number = request_data['phone_number']
-            group_title = request_data['group_title']
-            group_bio = request.json.get('group_bio') if 'group_bio' in request_data else ''
-
-            if len(username) == 0:
-                data = {"message": 'username can not be an empty string'}
-                return jsonify(data)
-
-            if len(phone_number) == 0:
-                data = {"message": 'phone_number can not be an empty string'}
-                return jsonify(data)
-
-            if len(group_title) == 0:
-                data = {"message": 'group_title can not be an empty string'}
-                return jsonify(data)
-
-            inserted_id = actions_collection.insert_one({
-                "username": username,
-                "phone_number": phone_number,
-                "group_title": group_title,
-                "group_bio": group_bio,
-                'status': 'waiting',
-                'type': task_type,
-                'insert_date': datetime.now()
-            }).inserted_id
 
             if inserted_id:
                 response = make_response(
-                    jsonify(
-                        {"message": '201 Group created', "severity": "danger"}
-                    ),
-                    201,
+                    jsonify({
+                        "message": '201 Channel created',
+                        "severity": "info"
+                        }),
+                    201
                 )
+
             else:
                 response = make_response(
-                    jsonify(
-                        {"message": '500 Group Not Created', "severity": "danger"}
-                    ),
+                    jsonify({
+                        "message": '500 Channel not created',
+                        "severity": "danger"
+                        }),
                     500
                 )
 
-        if (task_type == 3):
+        if task_type == 2:
             if 'group_title' not in request_data:
-                data = {"message": 'group_title can not be empty'}
-                return jsonify(data)
-
-            if 'channel_title' not in request_data:
-                data = {"message": 'channel_title can not be empty'}
-                return jsonify(data)
+                return jsonify({"message": 'Group title can not be empty'})
 
             if 'username' not in request_data:
-                data = {"message": 'username can not be empty'}
-                return jsonify(data)
+                return jsonify({"message": 'username can not be empty'})
 
             if 'phone_number' not in request_data:
-                data = {"message": 'phone_number can not be empty'}
-                return jsonify(data)
+                return jsonify({"message": 'Phone number can not be empty'})
+
+            username = request_data['username']
+            phone_number = request_data['phone_number']
+            group_title = request_data['group_title']
+            group_bio = request.json.get('group_bio') if 'group_bio' in request_data else ''
+
+            if len(username) == 0:
+                return jsonify({"message": 'username can not be an empty string'})
+
+            if len(phone_number) == 0:
+                return jsonify({"message": 'phone_number can not be an empty string'})
+
+            if len(group_title) == 0:
+                return jsonify({"message": 'group_title can not be an empty string'})
+
+            inserted_id = actions_collection.insert_one({
+                "username": username,
+                "phone_number": phone_number,
+                "group_title": group_title,
+                "group_bio": group_bio,
+                'status': 'waiting',
+                'type': task_type,
+                'insert_date': datetime.now()
+            }).inserted_id
+
+            if inserted_id:
+                response = make_response(
+                    jsonify({
+                        "message": '201 Group created',
+                        "severity": "danger"
+                        }),
+                    201
+                )
+
+            else:
+                response = make_response(
+                    jsonify({
+                        "message": '500 Group Not Created',
+                        "severity": "danger"
+                        }),
+                    500
+                )
+
+        if task_type == 3:
+            if 'group_title' not in request_data:
+                return jsonify({"message": 'group_title can not be empty'})
+
+            if 'channel_title' not in request_data:
+                return jsonify({"message": 'channel_title can not be empty'})
+
+            if 'username' not in request_data:
+                return jsonify({"message": 'username can not be empty'})
+
+            if 'phone_number' not in request_data:
+                return jsonify({"message": 'phone_number can not be empty'})
 
             username = request_data['username']
             phone_number = request_data['phone_number']
@@ -151,20 +142,16 @@ def create_ch_group():
             group_bio = request.json.get('group_bio') if 'group_bio' in request_data else ''
 
             if len(username) == 0:
-                data = {"message": 'username can not be an empty string'}
-                return jsonify(data)
+                return jsonify({"message": 'username can not be an empty string'})
 
             if len(phone_number) == 0:
-                data = {"message": 'phone_number can not be an empty string'}
-                return jsonify(data)
+                return jsonify({"message": 'phone_number can not be an empty string'})
 
             if len(group_title) == 0:
-                data = {"message": 'group_title can not be an empty string'}
-                return jsonify(data)
+                return jsonify({"message": 'group_title can not be an empty string'})
 
             if len(channel_title) == 0:
-                data = {"message": 'channel_title can not be an empty string'}
-                return jsonify(data)
+                return jsonify({"message": 'channel_title can not be an empty string'})
 
             inserted_id = actions_collection.insert_one({
                 "username": username,
@@ -180,16 +167,19 @@ def create_ch_group():
 
             if inserted_id:
                 response = make_response(
-                    jsonify(
-                        {"message": '201 Channel and Group created', "severity": "danger"}
-                    ),
-                    201,
+                    jsonify({
+                        "message": '201 Channel and Group created',
+                        "severity": "danger"
+                        }),
+                    201
                 )
+
             else:
                 response = make_response(
-                    jsonify(
-                        {"message": '500 Not Created', "severity": "danger"}
-                    ),
+                    jsonify({
+                        "message": '500 Not Created',
+                        "severity": "danger"
+                        }),
                     500
                 )
 
@@ -204,18 +194,15 @@ def create_ch_group():
 def add_user():
     request_data = request.get_json()
 
-    # if 'channel_id' or 'group_id' not in request_data:
-    #     data = {'message':'Enter a channel id or group id'}
-    #     return jsonify(data)
+    # if ('channel_id' not in request_data) and ('group_id' not in request_data):
+    #     return jsonify({'message': 'Enter a channel id or group id'})
 
     if request.method == 'POST':
         if 'username' not in request_data:
-            data = {'message': 'username can not be empty'}
-            return jsonify(data)
+            return jsonify({'message': 'username can not be empty'})
 
         if 'phone_number' not in request_data:
-            data = {"message": 'phone number can not be empty'}
-            return jsonify(data)
+            return jsonify({"message": 'phone number can not be empty'})
 
         username = request_data['username']
         phone_number = request_data['phone_number']
@@ -223,16 +210,13 @@ def add_user():
         channel_id = int(request_data['channel_id']) if 'channel_id' in request_data else 0
 
         if len(username) == 0:
-            data = {"message": 'username can not be an empty string'}
-            return jsonify(data)
+            return jsonify({"message": 'username can not be an empty string'})
 
         if len(phone_number) == 0:
-            data = {"message": 'phone_number can not be an empty string'}
-            return jsonify(data)
+            return jsonify({"message": 'phone_number can not be an empty string'})
 
         if len(group_id) == 0 and len(channel_id) == 0:
-            data = {"message": 'You must at least enter one id/channel or group'}
-            return jsonify(data)
+            return jsonify({"message": 'You must at least enter one id/channel or group'})
 
         inserted_id = actions_collection.insert_one({
             "username": username,
@@ -246,20 +230,25 @@ def add_user():
 
         if inserted_id:
             response = make_response(
-                jsonify(
-                    {"message": '200 user added', "severity": "info"}
-                ),
-                200,
+                jsonify({
+                    "message": '200 user added',
+                    "severity": "info"
+                    }),
+                200
             )
+
         else:
             response = make_response(
-                jsonify(
-                    {"message": '500 User Could Not Be Added', "severity": "danger"}
-                ),
+                jsonify({
+                    "message": '500 User Could Not Be Added',
+                    "severity": "danger"
+                    }),
                 500
             )
 
         return response
+
+
     else:
         return jsonify({'message': 'method not supported :('})
 
