be2f5b78078e0f6622a26433119363b9cd267eae
sina <sina.programerr@gmail.com>
2022-07-17 12:20:06 +0430

    Add check_attributes()  to  check request_data more efficiently

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, jsonify, request, make_response
 import bot
 
 
+def check_attributes(data: dict, attrs):
+    for attr in attrs:
+        if not data.get(attr, None):
+            return jsonify({'message': f"{attr} is invalid"})
+
+
 app = Flask(__name__)
 
 
@@ -22,29 +28,9 @@ def create():
 
     if request.method == 'POST':
         if task_type == 1:  # create channel
-            if 'username' not in request_data:
-                return jsonify({'message': 'username can not be empty'})
 
-            if 'phone_number' not in request_data:
-                return jsonify({"message": 'phone number can not be empty'})
-
-            if 'channel_title' not in request_data:
-                return jsonify({"message": 'channel can not be empty'})
-
-            username = request_data['username']
-            phone_number = request_data['phone_number']
-            channel_title = request_data['channel_title']
-            channel_bio = request_data['channel_bio'] if 'channel_bio' in request_data else ''
-
-            if len(username) == 0:
-                return jsonify({"message": 'username can not be an empty string'})
-
-            if len(phone_number) == 0:
-                return jsonify({"message": 'phone_number can not be an empty string'})
-
-            if len(channel_title) == 0:
-                return jsonify({"message": 'channel_title can not be an empty string'})
-
+            if result := check_attributes(request_data, ['username', 'phone_number', 'channel_title']):
+                return result
 
             channel_id = bot.create_channel(request_data)
 
@@ -68,29 +54,9 @@ def create():
                 )
 
         if task_type == 2:  # create group
-            if 'group_title' not in request_data:
-                return jsonify({"message": 'Group title can not be empty'})
 
-            if 'username' not in request_data:
-                return jsonify({"message": 'username can not be empty'})
-
-            if 'phone_number' not in request_data:
-                return jsonify({"message": 'Phone number can not be empty'})
-
-            username = request_data['username']
-            phone_number = request_data['phone_number']
-            group_title = request_data['group_title']
-            group_bio = request.json.get('group_bio') if 'group_bio' in request_data else ''
-
-            if len(username) == 0:
-                return jsonify({"message": 'username can not be an empty string'})
-
-            if len(phone_number) == 0:
-                return jsonify({"message": 'phone_number can not be an empty string'})
-
-            if len(group_title) == 0:
-                return jsonify({"message": 'group_title can not be an empty string'})
-
+            if result := check_attributes(request_data, ['username', 'phone_number', 'group_title']):
+                return result
 
             group_id = bot.create_group(request_data)
 
@@ -114,37 +80,9 @@ def create():
                 )
 
         if task_type == 3:  # create both
-            if 'group_title' not in request_data:
-                return jsonify({"message": 'group_title can not be empty'})
 
-            if 'channel_title' not in request_data:
-                return jsonify({"message": 'channel_title can not be empty'})
-
-            if 'username' not in request_data:
-                return jsonify({"message": 'username can not be empty'})
-
-            if 'phone_number' not in request_data:
-                return jsonify({"message": 'phone_number can not be empty'})
-
-            username = request_data['username']
-            phone_number = request_data['phone_number']
-            channel_title = request_data['channel_title']
-            channel_bio = request_data['channel_bio'] if 'channel_bio' in request_data else ''
-            group_title = request_data['group_title']
-            group_bio = request.json.get('group_bio') if 'group_bio' in request_data else ''
-
-            if len(username) == 0:
-                return jsonify({"message": 'username can not be an empty string'})
-
-            if len(phone_number) == 0:
-                return jsonify({"message": 'phone_number can not be an empty string'})
-
-            if len(group_title) == 0:
-                return jsonify({"message": 'group_title can not be an empty string'})
-
-            if len(channel_title) == 0:
-                return jsonify({"message": 'channel_title can not be an empty string'})
-
+            if result := check_attributes(request_data, ['username', 'phone_number', 'channel_title', 'group_title']):
+                return result
 
             channel_id, group_id = bot.create_both(request_data)
 
@@ -182,25 +120,16 @@ def add_user():
     if ('channel_id' not in request_data) and ('group_id' not in request_data):
         return jsonify({'message': 'Please enter a channel/group id'})
 
+
     if request.method == 'POST':
-        if 'username' not in request_data:
-            return jsonify({'message': 'username can not be empty'})
 
-        if 'phone_number' not in request_data:
-            return jsonify({"message": 'phone number can not be empty'})
+        if result := check_attributes(request_data, ['username', 'phone_number']):
+            return result
 
-        username = request_data['username']
-        phone_number = request_data['phone_number']
         group_id = int(request_data['group_id']) if 'group_id' in request_data else 0
         channel_id = int(request_data['channel_id']) if 'channel_id' in request_data else 0
 
-        if len(username) == 0:
-            return jsonify({"message": 'username can not be an empty string'})
-
-        if len(phone_number) == 0:
-            return jsonify({"message": 'phone_number can not be an empty string'})
-
-        if len(group_id) == 0 and len(channel_id) == 0:
+        if group_id == 0 and channel_id == 0:
             return jsonify({"message": 'You must at least enter one id/channel or group'})
 
         added_ids = bot.add_user(request_data)
